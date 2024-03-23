094ac6f5bdacbd392ffd75f884674f4a1630f04e
gmartinp03 <gmp@iastate.edu>
2024-03-22 21:16:07 -0500

    Finish Required Plots 3) and Cd calculation

--- Code/Lab7Analysis.m
+++ Code/Lab7Analysis.m
@@ -16,7 +16,7 @@ figure_count = 1;
 % .html
 mu_air = 18.18e-6; % [Pa*s]
 rho_air = 1.225; % [kg/m^3]
-C = 0.101; % [m]
+L = 0.101; % [m]
 % Curve fit equation for voltage (x) vs air speed (y) from Lab 6
 v_vs_U = @(x) -4982.7570*x.^4 + 24021.8937*x.^3 + -43230.9413*x.^2 + 34446.6867*x + -10248.1714;
 Cf = [34446.6867 -43230.9413 24021.8937 -4982.7570];
@@ -24,7 +24,11 @@ row = length(aoa);
 col = length(pos);
 volt = zeros(row, col); % [V] voltage
 U = zeros(row, col); % [m/s] velocity
-Ue = 1.32372*(15) -0.430966; % Freestream velocity estimated from Lab 2
+Ue = 1.32372*(15) -0.430966; % [m/s] Freestream velocity estimated from Lab 2
+d_m = .2*.0254; % [m] Distance between measurements
+
+%% Reynolds Number
+Re = rho_air * Ue * L / mu_air;
 
 %% Find avg. Voltage and Air Speed
 unzip(data_zip);
@@ -52,22 +56,23 @@ xlabel("Position [in]");
 ylabel("Velocity [m/s]");
 grid on;
 legend(aoa + "° AOA");
+%saveas(gcf, figure_dir + title_str + ".svg");
 
 %% Position vs. Voltage Graph
-figure(figure_count);
-figure_count = figure_count+1;
-for i = 1:row
-    plot(pos, volt(i,:));
-    hold on
-end
-fontname("Times New Roman");
-fontsize(12, "points");
-title_str = "Position vs. Anemometer Voltage at 4° to 16° AOA";
-title(title_str);
-xlabel("Position [in]");
-ylabel("Voltage [V]");
-grid on;
-legend(aoa + "° AOA");
+% figure(figure_count);
+% figure_count = figure_count+1;
+% for i = 1:row
+%     plot(pos, volt(i,:));
+%     hold on
+% end
+% fontname("Times New Roman");
+% fontsize(12, "points");
+% title_str = "Position vs. Anemometer Voltage at 4° to 16° AOA";
+% title(title_str);
+% xlabel("Position [in]");
+% ylabel("Voltage [V]");
+% grid on;
+% legend(aoa + "° AOA");
 
 % Boundry Layer determined from graph estimations (looking/making it up)
 delta = [0.4, 1, 1.5, 3]; % [in]
@@ -89,19 +94,27 @@ for i = 1:row
     xlabel("Y/\delta");
     ylabel("U/Ue");
     grid on;
+    %saveas(gcf, figure_dir + title_str + ".svg");
 end
 
 %% Normalized Position vs. Turbulence Intensity 
+turb_i = zeros(row,col);
 for i = 1:row
-    v_rms = rms(volt(i,:));
+    v_rms = zeros(1,col);
+    for j = 1:col
+        % find rms of raw data at each position
+        name = "./" + aoa(i) + "/" + pos(j) + "in.txt";
+        temp = readmatrix(name);
+        v = temp(4:1003,2);
+        v_rms(j) = rms(v);
+    end
     v_ = volt(i,:);
-    u_ = v_vs_U(v_);
-    u_rms = (Cf(1) + 2*Cf(2)*v_ + 3*Cf(3)*v_.^2 + 4*Cf(4)*v_.^3)*v_rms;
-    turb_i = u_rms./u_;
+    u_rms = (Cf(1) + 2*Cf(2)*v_ + 3*Cf(3)*v_.^2 + 4*Cf(4)*v_.^3).*v_rms;
+    turb_i(i,:) = u_rms./U(i,:);
 
     figure(figure_count)
     figure_count = figure_count+1;
-    plot(y_delta(i,:), turb_i);
+    plot(y_delta(i,:), turb_i(i,:));
     fontname("Times New Roman");
     fontsize(12, "points");
     title_str = "Y/\delta vs. Turbulence Intensity at "+ aoa(i)+"° AOA";
@@ -109,11 +122,65 @@ for i = 1:row
     xlabel("Y/\delta");
     ylabel("Turbulence Intensity");
     grid on;
+    %saveas(gcf, figure_dir + title_str + ".svg");
 end
 
 %% Vortex Shedding Frequency
+for i = 1:4
+    figure(figure_count)
+    figure_count = figure_count+1;
+    % Inside Wake
+    name = "./" + aoa(i) + "/" + pos(10) + "in.txt";
+    temp = readmatrix(name);
+    y = temp(4:1003,2);
+    Fs = 10000;
+    n = length(y);
+    Y = fft(y);
+    P2 = abs(Y/n); % Compute the two-sided spectrum
+    P1 = P2(1:n/2+1); % Compute the single-sided spectrum
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(n/2))/n; % Define the frequency domain 
+    % Plot the single-sided amplitude spectrum inside wake
+    plot(f,P1);
+    hold on
 
-dirs = {'4', '8', '12', '16'};  % Replace with your directories
+    % Outside Wake
+    name = "./" + aoa(i) + "/" + pos(1) + "in.txt";
+    temp = readmatrix(name);
+    y = temp(4:1003,2);
+    Fs = 10000;
+    n = length(y);
+    Y = fft(y);
+    P2 = abs(Y/n); % Compute the two-sided spectrum
+    P1 = P2(1:n/2+1); % Compute the single-sided spectrum
+    P1(2:end-1) = 2*P1(2:end-1);
+    f = Fs*(0:(n/2))/n; % Define the frequency domain 
+    % Plot the single-sided amplitude spectrum inside wake
+    plot(f,P1);
+
+    title_str ="Single-Sided Amplitude Spectrum at "+ aoa(i)+"° AOA";
+    title(title_str);
+    xlabel('f (Hz)');
+    ylabel('|P1(f)|');
+    ylim([0,.3]);
+    legend({'Inside Wake', 'Outisde Wake'})
+    %saveas(gcf, figure_dir + title_str + ".svg");
+end
+
+%% Momentum Thickness
+theta = zeros(1,row);
+C_D = zeros(1,row);
+for i = 1:row
+    for j = 10:20 
+        f1 = U(i,j)/Ue*(1- U(i,j)/Ue);
+        f2 = U(i,j+1)/Ue*(1-U(i,j+1)/Ue);
+        theta(i) = theta(i) + d_m*((f1 + f2)/2);
+    end
+    C_D(i) = 2 * theta(i) / L;
+end
+
+%% Delete unzipped data
+dirs = {'4', '8', '12', '16'};
 for i = 1:length(dirs)
       rmdir(dirs{i}, 's'); 
 end
